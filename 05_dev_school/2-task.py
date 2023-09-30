1fbaa21359cb70b841b2cf71f0810241bbd601fb
Denys <denis06699@gmail.com>
2023-09-30 22:11:18 +0300

    rebuild

--- 05_dev_school/2-task.py
+++
@@ -1,9 +0,0 @@
-"""
-Your task, is to calculate the minimal number of moves to win the game
-"Towers of Hanoi", with given number of disks.
-"""
-def search_count_move(count_disk):
-    return (2 ** count_disk) - 1
-
-a = 10
-print(search_count_move(a))
