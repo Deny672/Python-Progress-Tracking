3b6de5470a38ee4f389d6ac5fc36423efbdd6380
Denys <denis06699@gmail.com>
2024-10-30 17:08:11 +0200

    Added files

---
+++ 05_OOP/basic_customization/metaclass_example.py
@@ -0,0 +1,13 @@
+
+
+def create_class_point(name, base, attrs):
+    attrs.update({'MAX_COORD':100, 'MIN_COORD':0})
+    return type(name, base, attrs)
+
+class Point(metaclass=create_class_point):
+    def get_coords(self):
+        return (0, 0)
+
+pt = Point()
+print(pt.MAX_COORD)
+print(pt.get_coords())
